76168fa8ca7df931b63c4b9549634e5d03a562d1
Vixtor61 <00198117@uca.edu.sv>
2019-04-11 15:19:22 -0600

    f9ist

---
+++ app/src/main/java/com/naldana/ejemplo10/adapters/CurrencyAdapter.kt
@@ -0,0 +1,27 @@
+package com.naldana.ejemplo10.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class CurrencyAdapter : RecyclerView.Adapter<CurrencyAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
+
+        override fun onClick(v: View?) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+
+    }
+}
---
+++ app/src/main/java/com/naldana/ejemplo10/pojos/Currency.kt
@@ -0,0 +1,22 @@
+package com.naldana.ejemplo10.pojos
+
+
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Movie(
+
+    val id: String = "N/A",
+    val name: String = "N/A",
+    val country: String = "N/A",
+    val value: String = "N/A",
+    val value_us: String = "N/A",
+    val year:String = "N/A",
+    val review:String = "N/A",
+    val isAvailable:String = "N/A",
+    val img:String = "N/A"
+
+
+
+)
